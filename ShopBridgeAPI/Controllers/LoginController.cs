55f578ddaf31f665a711d814a9f0115585955d14
D6_63232_gaurav <gpchandan8995@gmail.com>
2023-02-10 11:41:55 +0530

    Login controller is added to authnticate user is valid or not  also view for login is added

---
+++ ShopBridgeAPI/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace ShopBridgeAPI.Controllers
+{
+    public class LoginController : Controller
+    {
+        InventoryEntities db=new InventoryEntities();
+        public ActionResult Signin()
+        {
+            return View("Signin");
+        }
+
+        public ActionResult afterSignin(Iuser user1)
+        {
+
+            List<Iuser> users = db.Iusers.ToList();
+            foreach (Iuser user in users)
+            {
+                if (user.Uemail == user1.Uemail && user.Upass == user1.Upass)
+                {
+                    FormsAuthentication.SetAuthCookie(user1.Uemail, false);
+                    return Redirect("/Home/Index");
+                }
+            }
+
+            ViewBag.Title = "Invalid username or password try again";
+            return View("Signin");
+        }
+    }
+}

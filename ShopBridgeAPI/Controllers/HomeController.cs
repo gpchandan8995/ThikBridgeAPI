160e5e0433c8318e2106a5a22cfbbac40bec22ed
D6_63232_gaurav <gpchandan8995@gmail.com>
2023-02-10 11:56:43 +0530

    to add new iteam new action were created in home controller and  List is displayed on index page

--- ShopBridgeAPI/Controllers/HomeController.cs
+++ ShopBridgeAPI/Controllers/HomeController.cs
@@ -6,13 +6,29 @@ using System.Web.Mvc;
 
 namespace ShopBridgeAPI.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        InventoryEntities db =new InventoryEntities();
         public ActionResult Index()
         {
-            return View();
+            var dataList= db.Items.ToList();    
+            return View(dataList);
         }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            ViewBag.Message = " Item added in Inventory";
+            return View("index");
+        }
 
     }
 }

76e6643b61f4b22f31019f398dea318e866e3448
D6_63232_gaurav <gpchandan8995@gmail.com>
2023-02-10 11:25:14 +0530

    used table first approach for design of framework tables were created and using ADO.net framework classes mapping to that tables were built

---
+++ dbo.Item.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Item] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Iname]        NVARCHAR (50) NOT NULL,
+    [Idescription] NVARCHAR (50) NULL,
+    [Iprice]       INT           NOT NULL,
+    [IaddDate]     DATE      NOT NULL,
+    [IexpDate]     DATE          NULL,
+    [Iavailable]   NCHAR (10)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
---
+++ dbo.user.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Iuser] (
+    [Uid]    INT           IDENTITY (1, 1) NOT NULL,
+    [Uemail] NVARCHAR (50) NOT NULL,
+    [Upass]  VARCHAR (50)  NOT NULL,
+    [Urole]  SMALLINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Uid] ASC)
+);
